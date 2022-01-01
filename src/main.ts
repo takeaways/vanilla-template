ae057ff0593865aaa9db1620ecbabb1baa75c489
JangGeonil <wkdrjsdlf2@gmail.com>
2022-01-01 21:16:08 +0900

    team section

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 import './style.css'
 
 
+document.querySelectorAll('.story-btn').forEach((btnElement)=>{
+  btnElement.addEventListener('click', ()=>{
+    btnElement.classList.toggle('change');
+    btnElement.nextElementSibling?.classList.toggle('change');
+  })
+})
